d5ab757987b1309317f60d19c0c40cb39c08dbb0
Abhay7520 <abhayy7520@gmail.com>
2026-05-09 22:37:02 +0530

    undo files

---
+++ mini_backend/database.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
